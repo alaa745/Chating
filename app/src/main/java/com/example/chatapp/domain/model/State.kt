b5d73df505d5f9448d7b8cae0acca601cb899220
Alaa Hosni <alaahosnii1000@gmail.com>
2024-05-27 13:49:43 +0300

    implemented send verification code use case

---
+++ app/src/main/java/com/example/chatapp/domain/model/State.kt
@@ -0,0 +1,5 @@
+package com.example.chatapp.domain.model
+
+data class State(
+    val name: String
+)
--- app/src/main/java/com/example/chatapp/presentation/authScreen/AuthViewModel.kt
+++ app/src/main/java/com/example/chatapp/presentation/authScreen/AuthViewModel.kt
@@ -1,6 +1,40 @@
 package com.example.chatapp.presentation.authScreen
 
+import android.app.Activity
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.chatapp.domain.usecase.SendVerificationCodeUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class AuthViewModel: ViewModel() {
+@HiltViewModel
+class AuthViewModel @Inject constructor(private val sendVerificationCodeUseCase: SendVerificationCodeUseCase): ViewModel() {
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> get() = _isLoading
+
+    private val _isCodeSent = MutableStateFlow(false)
+    val isCodeSent: StateFlow<Boolean> get() = _isCodeSent
+
+    private val _message = MutableStateFlow("")
+    val message: StateFlow<String> get() = _message
+    fun sendVerificationCode(phoneNumber: String , activity: Activity){
+        _isLoading.value = true
+        viewModelScope.launch {
+            sendVerificationCodeUseCase.invoke(phoneNumber = phoneNumber , activity = activity).collect{ result ->
+                _isLoading.value = false
+                result.onSuccess {
+                    _message.value = "Code Sent"
+                    _isCodeSent.value = true
+                }
+                result.onFailure {
+                    _message.value = it.message ?: "Error Sending Code"
+                    _isCodeSent.value = false
+                }
+            }
+        }
+    }
 }
